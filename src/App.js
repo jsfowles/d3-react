cb1e6f15ca0f70187f6b952461bf3b2637917931
Jacob Fowles <js.fowles@gmail.com>
2017-09-26 08:36:51 -0600

    added csv

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
-import ProgressArc from './ProgressArc';
+import Arc from './Arc';
+import * as d3 from "d3";
+
 
 class App extends Component {
   constructor(props) {
-     super(props);
-     this.state = {percentComplete: 0.3};
-     this.togglePercent = this.togglePercent.bind(this);
-   }
+    super(props);
+    this.state = { percentComplete: 0.2 };
+    this.togglePercent = this.togglePercent.bind(this);
+  }
 
   togglePercent() {
-    const percentage = this.state.percentComplete === 0.3 ? 0.7 : 0.3;
-    this.setState({percentComplete: percentage});
+    const percentage = this.state.percentComplete === 0.2 ? 0.8 : 0.2;
+    this.setState({ percentComplete: percentage });
   }
+
+  componentWillMount() {
+    d3.csv("file.csv", function(error, data) {
+      if (error) throw error;
+      console.log(data.columns); 
+    });
+  }
+
 
   render() {
     console.log(this.state.percentComplete);
     return (
       <div>
-        <a onClick={this.togglePercent}>Toggle Arc</a>
-        <ProgressArc
+        <button onClick={this.togglePercent}>Toggle</button>
+        <Arc
+          text="Gender"
           height={300}
           width={300}
           innerRadius={100}
-          outerRadius={110}
+          outerRadius={125}
           id="d3-arc"
           backgroundColor="#e6e6e6"
-          foregroundColor="#00ff00"
+          foregroundColor="#E80000"
           percentComplete={this.state.percentComplete}
+          duration={1000}
         />
       </div>
     );
---
+++ src/Arc.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+
+class Arc extends Component {
+  componentDidMount() {
+    this.drawArc();
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidUpdate() {
+    this.redrawArc();
+  }
+
+  drawArc() {
+    const context = this.setContext();
+    this.setBackground(context);
+    this.setForeground(context);
+    this.updatePercent(context);
+    this.setText(context);
+  }
+
+  updatePercent(context) {
+    return this.setForeground(context)
+      .transition()
+      .duration(this.props.duration)
+      .call(this.arcTween, this.tau * this.props.percentComplete, this.arc());
+  }
+
+  arcTween(transition, newAngle, arc) {
+    transition.attrTween("d", d => {
+      const interpolate = d3.interpolate(d.endAngle, newAngle);
+      const newArc = d;
+      return t => {
+        newArc.endAngle = interpolate(t);
+        return arc(newArc);
+      };
+    });
+  }
+
+  redrawArc() {
+    const context = d3.select("#d3-arc");
+    context.remove();
+    this.drawArc();
+  }
+
+  setContext() {
+    const { height, width, id } = this.props;
+    return d3
+      .select(this.refs.arc)
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width)
+      .attr("id", id)
+      .append("g")
+      .attr("transform", `translate(${height / 2}, ${width / 2})`);
+  }
+
+  setText(context) {
+    console.log(this.refs.arc);
+    return context
+      .append("text")
+        .text(this.props.text)
+        .attr("x", -40)
+        .attr("y", 10)
+          .style("font-family", "Gotham")
+          .style("font-size", 18)
+          .style("text-transform", "uppercase");
+  }
+
+  setBackground(context) {
+    return context
+      .append("path")
+      .datum({ endAngle: this.tau })
+      .style("fill", this.props.backgroundColor)
+      .attr("d", this.arc());
+  }
+
+  setForeground(context) {
+    return context
+      .append("path")
+      .datum({ endAngle: 2 })
+      .style("fill", this.props.foregroundColor)
+      .attr("d", this.arc());
+  }
+
+  tau = Math.PI * 2;
+
+  arc() {
+    return d3
+      .arc()
+      .innerRadius(this.props.innerRadius)
+      .outerRadius(this.props.outerRadius)
+      .startAngle(0);
+  }
+
+  render() {
+    return <div ref="arc" />;
+  }
+}
+
+export default Arc;
